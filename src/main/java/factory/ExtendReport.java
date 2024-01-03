596beeaefd64d37a2ee63f3a57a091828fa4440f
Zakir <79053961+zakirizu@users.noreply.github.com>
2024-01-03 14:08:58 +0530

    Update

--- src/main/java/factory/ExtendReport.java
+++ src/main/java/factory/ExtendReport.java
@@ -1,19 +1,14 @@
 package factory;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtendReport {
-	
-	
 	public static ExtentSparkReporter reporter;
 	public static ExtentReports extent;
-		
-
 	public static ExtentReports extendReportsUtils() {
 		String Reportspath 				= factory.Constants.REPORTS;
-		String Env 						= factory.Constants.Environment;
+		String Env 							= factory.Constants.Environment;
 		
 		reporter 	= new ExtentSparkReporter(Reportspath);
 		reporter.config().setDocumentTitle("Bluestone Automation");
